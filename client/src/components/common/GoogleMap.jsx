14ae9312db79d19fb705e0818d9009701ea51541
Guren25 <felixjuaton87@gmail.com>
2025-07-12 18:04:21 +0800

    Update styles for Contact and Footer components; enhance GoogleMap integration
    - Added padding to the contact content for improved spacing.
    - Increased map height in Contact.css and adjusted GoogleMap component to utilize full height.
    - Updated Footer styles to remove borders and improve hover effects for links and social icons.

--- client/src/components/common/GoogleMap.jsx
+++ client/src/components/common/GoogleMap.jsx
@@ -4,11 +4,11 @@ const GoogleMap = () => {
   const mapUrl = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.4772834469187!2d121.05335417469791!3d14.579494185999897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c98ce637360d%3A0xfd26309ab797859b!2sPakBet%20TV%20Feng%20Shui%20Consultancy%20Inc.!5e0!3m2!1sen!2sph!4v1709706844544!5m2!1sen!2sph";
 
   return (
-    <div className="google-map">
+    <div className="google-map" style={{ height: '100%' }}>
       <iframe
         src={mapUrl}
         width="100%"
-        height="250"
+        height="100%"
         style={{ border: 0 }}
         allowFullScreen=""
         loading="lazy"
